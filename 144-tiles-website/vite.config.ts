1ab85ce2e94e1c0cf278558a40c9f5e10590255f
michael-carbon <michael@carboncre.ai>
2026-05-28 21:18:24 -0500

    fix css

--- 144-tiles-website/vite.config.ts
+++ 144-tiles-website/vite.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: { open: true },
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
-    : "/",
+  base: "/",
 });
